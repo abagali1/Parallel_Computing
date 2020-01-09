f1e709bb8dc091f86fe52b155982385d6990564b
Anup Bagali <anup.bagali1@gmail.com>
2020-01-09 14:12:05 -0500

    did stuffs

--- SEM1/lab11/main.c
+++ SEM1/lab11/main.c
@@ -1,37 +1,37 @@
-//
-// Torbert, 8 February 2016
-//
 #include <stdio.h>
-//
+
 #define M 640
 #define N 480
-//
+#define MAX 1000
+
 int main(void)
 {
-   int rgb[N][M][3] ; // red-green-blue for each pixel
-   //
-   int y , x ;
-   //
-   FILE* fout ;
-   //
+   int rgb[N][M][3];
+   int y , x;
+   FILE* fout;
+   double palette[MAX];
+
    for( y = 0 ; y < N ; y++ )
    {
       for( x = 0 ; x < M ; x++)
-      {
-         rgb[y][x][0] = 0   ; // red
-         rgb[y][x][1] = 255 ; // green
-         rgb[y][x][2] = 0   ; // blue
+      {  
+         double x0 = (x*4/M)-2.0;
+         double y0 = (y*3/N)-1.5;
+         double x = 0;
+         double y = 0;
+         int i = 0;
+         while(x*x + y*y <= 4 && i < MAX){
+            double temp = x*x - y*y + x0;
+            y = 2*x*x + y0;
+            x = temp;
+            i++;
+         }
       }
    }
-   //
-   //
-   //
-   fout = fopen( "allgreen.ppm" , "w" ) ;
-   //
+   fout = fopen( "ms.ppm" , "w" ) ;
    fprintf( fout , "P3\n" ) ;
    fprintf( fout , "%d %d\n" , M , N ) ;
    fprintf( fout , "255\n" ) ;
-   //
    for( y = 0 ; y < N ; y++ )
    {
       for( x = 0 ; x < M ; x++)
@@ -41,9 +41,5 @@ int main(void)
       }
    }
    fclose( fout ) ;
-   //
    return 0 ;
 }
-//
-// end of file
-//
