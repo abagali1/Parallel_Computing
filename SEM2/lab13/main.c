f670f6bb938919c259f0154aeca668b1484c3024
Anup Bagali <anup.bagali1@gmail.com>
2020-02-06 13:12:57 -0500

    finished flat

--- SEM2/lab13/main.c
+++ SEM2/lab13/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 
@@ -89,7 +90,7 @@ void init(Sphere * a) {
     a[3].h.b = 0;
 }
 
-void normalize(Vector * v) {
+void normalize(Vector* v) {
     double mag = sqrt(pow(v->x, 2) + pow(v->y, 2) + pow(v->z, 2));
     v->x /= mag;
     v->y /= mag;
@@ -130,6 +131,30 @@ Vector scalar_multiply(Vector a, double scalar) {
     };
 }
 
+bool cast(Sphere sphere, Vector ray, double* t){
+    Vector v = subtract_vector(eye, sphere.c);
+    Vector d = ray;
+    double a = 1.0;
+    double b = 2.0*dotp(v, d);
+    double c = dotp(v,v) - sphere.r*sphere.r;
+
+    double det = pow(b,2) - 4.0*a*c;
+
+                    
+    if(det<0){
+        return false;
+    }
+    double det_sqrt = sqrt(det);
+    double t_plus = (-b + det_sqrt)/2.0;
+    double t_minus = (-b - det_sqrt)/2.0;
+
+    *t = (t_minus > 0 && t_plus >0) ?
+            min(t_minus, t_plus) :
+            max(t_minus, t_plus);
+
+    return *t > 0;
+}
+
 int main(void) {
     FILE * fout;
     Color ** rgb = malloc(sizeof(Color * ) * N);
@@ -143,8 +168,8 @@ int main(void) {
     double aspect_ratio = (1.0 * M) / (1.0 * N);
     for (int Py = 0; Py < N; Py++) {
         for (int Px = 0; Px < M; Px++) {
-            double x = (Px + 0.5) / (1.0 * M);
-            double y = ((N - y) + 0.5) / (1.0 * N);
+            double x = ((Px + 0.5) / (1.0 * M))*aspect_ratio;
+            double y = (((N - Py) + 0.5) / (1.0 * N));
             Vector ray = create_vector(eye, (Vector) {
                 .x = x,
 				.y = y,
@@ -154,44 +179,21 @@ int main(void) {
 	        Sphere min_sphere;
 			for(int s=0;s < 4; s++){ // d = ray v = sphere vector
 				Sphere sphere = spheres[s];
-				Vector v = subtract_vector(eye, sphere.c);
-                Vector d = ray;
-                double a = 1.0;
-                double b = 2.0*dotp(v, d);
-                double c = dotp(v,v) - sphere.r*sphere.r;
-
-                double det = pow(b,2) - 4*a*c;
-                                
-				if(det<0){
-					continue;
-				}
-                double det_sqrt = sqrt(det);
-                double t_plus = (-b + det_sqrt)/2.0;
-                double t_minus = (-b - det_sqrt)/2.0;
-                
                 double t;
-                if(t_plus >0 && t_minus > 0){
-				    t = min(t_plus, t_minus);
-                }else if(t_plus > 0 && t_minus < 0){
-                    t = t_plus;
-                }else if(t_plus < 0 && t_minus > 0){
-                    t = t_minus;
-                }else{
-                    continue;
-                }
 
-				if(t < t_min){
-					t_min = t;
-					min_sphere = sphere;
-				}
+                if(cast(sphere, ray, &t)){
+                    if(t>t_min){
+                        continue;
+                    }
+                    t_min = t;
+                    min_sphere = sphere;
+                }
 			}
             if(t_min == INFINITY){
                 rgb[Py][Px] = BLACK;
             }else{
-			    rgb[Py][Px]=min_sphere.h;
+                rgb[Py][Px] = min_sphere.h;
             }
-            t_min = INFINITY;
-            //min_sphere = spheres[1];
         }
     }
 
